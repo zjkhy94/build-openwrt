75e3d223b6a30221224c18019e1e47c2666047fe
zjkhy94 <zjkhy94@gmail.com>
2024-03-17 09:21:57 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -41,6 +41,8 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Add third-party software packages (The entire repository)
 # git clone https://github.com/libremesh/lime-packages.git package/lime-packages
+git clone src-git helloworld https://github.com/fw876/helloworld.git /package/helloworld
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # Add third-party software packages (Specify the package)
 # svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
 # Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
